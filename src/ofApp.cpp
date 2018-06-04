0e127c2fbe0d5f42e6342b303692a1d22d488b20
wezside <wesley@wezside.co.za>
2018-06-04 22:24:34 +0200

    Updated with Hooke's law spring physics

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,19 +8,26 @@ void ofApp::setup()
     path.setStrokeWidth(10);
 
  	ofEnableDepthTest();
-    int yoffset = 0;
+    int yoffset = -100;
 	baseNode.setPosition(0, yoffset, 0);
     childNode.setParent(baseNode);
-    childNode.setPosition(0, yoffset + 100, 0);
+    childNode.setPosition(0, yoffset + 220, 0);
     grandChildNode.setParent(childNode);
-    grandChildNode.setPosition(-100,yoffset+200,0);
+    grandChildNode.setPosition(0,yoffset+200,0);
 }
+
+
+ofVec3f velocity;
+ofVec3f target = ofVec3f(0, 100, 0);
+
+bool mousedown = false;
+bool drawNodes = true;
 
 //--------------------------------------------------------------
 void ofApp::update()
 {
-    baseNode.tiltDeg(sin(ofGetElapsedTimef() + 10) / 2.0);
-    childNode.tiltDeg(sin(ofGetElapsedTimef() + 10) / 5.0);
+    // baseNode.tiltDeg(sin(ofGetElapsedTimef() + 10) / 2.0);
+    // childNode.tiltDeg(sin(ofGetElapsedTimef() + 10) / 5.0);
     
     points.clear();
     points.push_back(baseNode.getGlobalPosition());
@@ -34,34 +41,66 @@ void ofApp::update()
     {
         path.curveTo(points[i].x, points[i].y, points[i].z);
     }
+ 	
+ 	//Constants
+    float dt = 1.0 / 60.0;         //Time step
+    float mass = 0.1;              //Mass of a ball
+    float rubberLen = 0.0;       //Segment's length
+    float k = 15.0;                 //Segment's stiffness
+    float damping = 0.5;
+    ofVec3f g( 9.8, 0.0, 0.0 );         //Gravity force
+    float childScale = 0.3;
+    
+    //Compute Hooke's force
+    ofVec3f delta = grandChildNode.getGlobalPosition() - target;
+    if (!mousedown)
+    {
+	    float len = delta.length();   //Vector's length
+	    float hookeValue = k * (len - rubberLen);
+	    delta.normalize();            //Normalize vector's length
+	    ofVec3f hookeForce = delta * (-hookeValue);
+	    ofVec3f dampingForce = damping * velocity;
 
-    grandChildNode.move(sin(ofGetElapsedTimef() + 20), 0, 0);
-    childNode.move(sin(ofGetElapsedTimef() + 10), 0, 0);
+	    //Update velocity and pos
+	    ofVec3f force = hookeForce + g - dampingForce;  //Resulted force
+	    ofVec3f a = force / mass;        //Second Newton's law
+	    velocity += a * dt;              //Euler method
+	    ofVec3f result = velocity * dt;
+	    // grandChildNode.move(result.x, 0, 0);            //Euler method    	
+	    grandChildNode.move(result);            //Euler method    	
+	    childNode.move(result * childScale);
+    }
+    else 
+    {
+    	childNode.move(0, 0, (ofGetMouseX() - ofGetWidth() * 0.5) * childScale / 100.0);
+    	grandChildNode.move(0, 0, (ofGetMouseX() - ofGetWidth() * 0.5) / 100.0);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw()
 {
-	ofBackground(0);
+	ofBackground(55);
 
    	cam.begin();
-    // baseNode.draw();
-    // childNode.draw();
-    // grandChildNode.draw();
 
-   	ofSetColor(255, 0, 0);
-   	ofDrawCircle(baseNode.getGlobalPosition(), 10);
-   	ofDrawCircle(childNode.getGlobalPosition(), 10);
-   	ofDrawCircle(grandChildNode.getGlobalPosition(), 10);
+   	ofSetColor(255);
+   	if (drawNodes)
+   	{
+	   	ofDrawCircle(baseNode.getGlobalPosition(), 5);
+	   	ofSetColor(255, 0, 0);
+	   	ofDrawCircle(childNode.getGlobalPosition(), 5);
+	   	ofSetColor(255);
+	   	ofDrawCircle(grandChildNode.getGlobalPosition(), 5);
+   	}
 
-    ofSetColor(255);
 	path.draw();
 	cam.end();
 	
 
  /*   ofMesh meshy;
     meshy.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
-	float widthSmooth = 10;
+	float widthSmooth = 10;	
     float angleSmooth;
     for (int i = 0;  i < path.getOutline().getVertices().size(); i++)
     {
@@ -113,7 +152,10 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+	switch (key)
+	{
+		case 'n': drawNodes = !drawNodes; break;
+	}
 }
 
 //--------------------------------------------------------------
@@ -128,12 +170,13 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+	mousedown = true;
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+	mousedown = false;
+	// target = ofVec3f(ofGetMouseX(), 200, 0);
 }
 
 //--------------------------------------------------------------
